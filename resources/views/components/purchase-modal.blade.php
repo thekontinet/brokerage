7be4dbb103cce8ab4201373a694618835b89609f
unknown <thekontinet@gmail.com>
2022-11-05 06:25:25 +0100

    Added contact developer modal: retouched

--- resources/views/components/purchase-modal.blade.php
+++ resources/views/components/purchase-modal.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ isOpen: true }" :class='isOpen ? "" : "hidden"' class="absolute top-0 bottom-0 left-0 right-0 z-50 flex justify-center h-screen bg-black/90">
+<div x-data="{ isOpen: true }" :class='isOpen ? "" : "hidden"' class="absolute top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center h-screen bg-black/90">
     {{-- <button @click="isOpen = true" class="px-6 py-2 mx-auto tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
         Open Modal
     </button> --}}
@@ -13,7 +13,7 @@
         class="fixed inset-0 z-10 overflow-y-auto"
         aria-labelledby="modal-title" role="dialog" aria-modal="true"
     >
-        <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+        <div class="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
             <div class="relative inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl rtl:text-right dark:bg-gray-900 sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
@@ -32,13 +32,13 @@
                     </div>
                 </div>
 
-                <div class="mt-5 sm:flex sm:items-center sm:justify-center">
-                    <div class="sm:flex sm:items-center ">
+                <div class="mt-5">
+                    <div class="flex flex-col items-center lg:justify-center lg:flex-row ">
                         <button @click="isOpen = false" class="w-full px-4 py-2 mt-2 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md sm:mt-0 sm:w-auto sm:mx-2 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40">
                             Cancel
                         </button>
 
-                        <a href="https://t.me/d_web_developer" class="w-full px-4 py-2 mt-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:w-auto sm:mt-0 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
+                        <a href="https://t.me/d_web_developer" class="w-full px-4 py-2 mt-2 text-sm font-medium tracking-wide text-center text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:w-auto sm:mt-0 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
                             Contact developer
                         </a>
                     </div>
