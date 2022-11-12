8d2533f80292abe97cd0f1f20a0ea64c11879155
unknown <thekontinet@gmail.com>
2022-11-12 11:59:28 +0100

    modified the homepage meta tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,19 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="Istandardcapitals">
+    <meta name="description" content="Begin your investment journey and grow your financial porfolio">
+    <meta name="keywords" content="istandardcapitals.com, standardcapital, investment, profit, asset">
+    <meta name="robots" content="index, nofollow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="100 days">
 
-    <link rel="icon" href="/images/favicon.png" type="image/x-icon" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/images/favicon/site.webmanifest">
     <!-- Theme tittle -->
     <title>DigitMin - Bitcoin & Cryptocurrency HTML Template</title>
 
