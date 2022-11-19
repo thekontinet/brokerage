1fbc38410bcd51517a8b889d0dd2690d548db5f7
unknown <thekontinet@gmail.com>
2022-11-19 07:18:41 +0100

    enabled imperonation feature

--- app/Http/Controllers/ImpersonatorController.php
+++ app/Http/Controllers/ImpersonatorController.php
@@ -13,7 +13,7 @@ class ImpersonatorController extends Controller
             'user_id' => ['required', 'exists:users,id']
         ]);
         $request->session()->put(['impersonate' => $request->user_id, 'impersonator_id' => auth()->id()]);
-        return Auth::loginUsingId($request->user_id);
+        return auth('web')->loginUsingId($request->user_id);
         return redirect()->route('dashboard');
     }
 
@@ -21,7 +21,7 @@ class ImpersonatorController extends Controller
     {
         $user_id = session('impersonator_id');
         session()->forget(['impersonate', 'impersonator_id']);
-        Auth::loginUsingId($user_id);
+        auth('web')->loginUsingId($user_id);
         return redirect()->route('dashboard');
     }
 }
